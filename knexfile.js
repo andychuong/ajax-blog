65c675dedbfbc68e94fcff3cff44ba16e2fed86b
Andy Chuong <andy.chuong@colorado.edu>
2018-10-18 15:04:30 -0600

    heroku database url

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,6 @@ module.exports = {
   test: {},
   production: {
     client: 'pg',
-    connection: ''
+    connection: 'postgres://bnrdptlockdtcv:1feaf7477ec255cb30aa303a11931f7196c10891b2a7772cd53a118f880f2df7@ec2-54-243-61-194.compute-1.amazonaws.com:5432/dd6h456uoib81s'
   }
 }
